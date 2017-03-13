55d6865ba416ca16a378b7489d50f0143ff0e5fc
zeiqie <dangdangka@163.com>
2017-03-13 15:50:03 +0800

    create models in chapter 2.

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = '91c9ea6d04baa22518c048a3e726f220'
